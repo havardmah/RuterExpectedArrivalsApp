bce39cd7975e21635b747021f96a25156b286aec
Håvard Melvin Hjelvik <development@hjelvik.net>
2017-01-06 00:40:53 +0100

    rebuilt html for new server-side data-handling

---
+++ js/app_v2.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Håvard on 03.01.2017.
+ */
+var ruterApp = angular.module("ruterApp", []);
+
+ruterApp.filter('timeCalculate', function () {
+    return function (x) {
+        var time = new Date();
+        var receivedTime = new Date(x);
+        var calculatedTime = Math.abs(time - receivedTime);
+        var minutesLeft = (calculatedTime/1000/60) << 0;
+
+        if (minutesLeft == 0) return "Nå";
+        return minutesLeft + " min";
+    };
+}); // End timeCalculate
+
+ruterApp.controller("RuterController", ["$http", function ($http) {
+    var _this = this;
+
+    _this.getBusDepartures = function () {
+        var apiUrl = "proxy_v2.php";
+
+        $http
+            .get(apiUrl)
+            .then(
+                // Success
+                function (response) {
+                    _this.response = response.data;
+                },
+                // Error
+                function (response) {
+                    console.log("Error", response);
+                }
+            ); // End http
+
+    } // End getBusDepartures
+
+    _this.getBusDepartures();
+    setInterval(_this.getBusDepartures, 5000);
+
+}]); // End RuterController
